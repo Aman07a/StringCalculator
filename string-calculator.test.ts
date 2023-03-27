396c82a697c26830bb7d71a77f84612e80b53198
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-27 11:37:00 +0200

    Worked Example: The String Calculator

--- string-calculator.test.ts
+++ string-calculator.test.ts
@@ -1,26 +1,12 @@
-// Theory to remember:
-// - The One to Many green bar pattern
-// - The Obvious green bar pattern
-// - The Backout green bar pattern
-// - The Learning Test green bar pattern
-// - The TDD Gears Model
-
 import { StringCalculator } from "./string-calculator";
-
-// String Kata first step:
-// 1. Create a simple String calculator with a function: add(input: string): number
-//  1.1. The add() function can take 0, 1 or 2 numbers, and will return their sums
-//      (for an empty string it will return 0) for example "" or "1" or "1,2"
-//  1.2. Start with the simplest test case of an empty string, then move to one and two numbers
-// 2. Allow the add() function to handle an unknown amount of numbers
 
 describe("string-calculator", () => {
   describe("add", () => {
-    test("add given empty string should return 0", () => {
+    test("given empty string should return 0", () => {
       // Arrange
       const input = "";
       const expected = 0;
-      const sut = new StringCalculator();
+      const sut = createSut();
       // Act
       const actual = sut.add(input);
       // Assert
@@ -34,7 +20,7 @@ describe("string-calculator", () => {
         { input: "300", expected: 300 },
       ])("input: $input", ({ input, expected }) => {
         // Arrange
-        const sut = new StringCalculator();
+        const sut = createSut();
         // Act
         const actual = sut.add(input);
         // Assert
@@ -49,7 +35,7 @@ describe("string-calculator", () => {
         { input: "234,754", expected: 988 },
       ])("input: $input", ({ input, expected }) => {
         // Arrange
-        const sut = new StringCalculator();
+        const sut = createSut();
         // Act
         const actual = sut.add(input);
         // Assert
@@ -64,7 +50,105 @@ describe("string-calculator", () => {
         { input: "12,148,1000", expected: 1160 },
       ])("input: $input", ({ input, expected }) => {
         // Arrange
-        const sut = new StringCalculator();
+        const sut = createSut();
+        // Act
+        const actual = sut.add(input);
+        // Assert
+        expect(actual).toBe(expected);
+      });
+    });
+
+    describe("new line as delimiter", () => {
+      test("1,2\\n3", () => {
+        // Arrange
+        const input = "1,2\n3";
+        const expected = 6;
+        const sut = createSut();
+        // Act
+        const actual = sut.add(input);
+        // Assert
+        expect(actual).toBe(expected);
+      });
+      test("1\\n2,3,4\\n50", () => {
+        // Arrange
+        const input = "1\n2,3,4\n50";
+        const expected = 60;
+        const sut = createSut();
+        // Act
+        const actual = sut.add(input);
+        // Assert
+        expect(actual).toBe(expected);
+      });
+    });
+
+    describe("custom delimiter", () => {
+      test("//;\\n1;2", () => {
+        // Arrange
+        const input = "//;\n1;2";
+        const expected = 3;
+        const sut = createSut();
+        // Act
+        const actual = sut.add(input);
+        // Assert
+        expect(actual).toBe(expected);
+      });
+      test("//@\\n2@20@50@3", () => {
+        // Arrange
+        const input = "//@\n2@20@50@3";
+        const expected = 75;
+        const sut = createSut();
+        // Act
+        const actual = sut.add(input);
+        // Assert
+        expect(actual).toBe(expected);
+      });
+    });
+
+    describe("throws on negative numbers", () => {
+      describe("one negative number in input", () => {
+        test.each([
+          { input: "1,2,-3", expected: "negatives not allowed: -3" },
+          { input: "10,-20,30", expected: "negatives not allowed: -20" },
+          { input: "-1000,20,2", expected: "negatives not allowed: -1000" },
+        ])("input: $input", ({ input, expected }) => {
+          // Arrange
+          const sut = createSut();
+          // Act/Assert
+          expect(() => {
+            sut.add(input);
+          }).toThrow(expected);
+        });
+      });
+      describe("many negative numbers in input", () => {
+        test.each([
+          { input: "1,-2,-3", expected: "negatives not allowed: -2,-3" },
+          {
+            input: "-10,-20,-30",
+            expected: "negatives not allowed: -10,-20,-30",
+          },
+          {
+            input: "-1000,-20,2,-30",
+            expected: "negatives not allowed: -1000,-20,-30",
+          },
+        ])("input: $input", ({ input, expected }) => {
+          // Arrange
+          const sut = createSut();
+          // Act/Assert
+          expect(() => {
+            sut.add(input);
+          }).toThrow(expected);
+        });
+      });
+    });
+
+    describe("large numbers (>1000) should be filtered out", () => {
+      test.each([
+        { input: "1,2,1001", expected: 3 },
+        { input: "1,2,1000", expected: 1003 },
+        { input: "1000,1001,999", expected: 1999 },
+      ])("input: $input", ({ input, expected }) => {
+        // Arrange
+        const sut = createSut();
         // Act
         const actual = sut.add(input);
         // Assert
@@ -72,4 +156,8 @@ describe("string-calculator", () => {
       });
     });
   });
+
+  function createSut() {
+    return new StringCalculator();
+  }
 });
--- string-calculator.ts
+++ string-calculator.ts
@@ -3,12 +3,41 @@ export class StringCalculator {
     if (input.length === 0) {
       return 0;
     }
-    if (input.includes(",")) {
-      return input
-        .split(",")
-        .map((s) => Number.parseInt(s))
-        .reduce((n, total) => total + n);
+    let delimiter: string | RegExp = /,|\n/;
+    if (this.hasCustomDelimiter(input)) {
+      delimiter = this.extractCustomDelimiter(input);
+      input = this.removeCustomDelimiterHeader(input);
     }
-    return Number.parseInt(input);
+    let parsedNumbers = this.parseNumbers(input, delimiter);
+    this.throwIfAnyNegativeNumbers(parsedNumbers);
+    parsedNumbers = parsedNumbers.filter((n) => n <= 1000);
+    return this.sumTotal(parsedNumbers);
+  }
+
+  private sumTotal(parsedNumbers: number[]) {
+    return parsedNumbers.reduce((n, total) => total + n);
+  }
+
+  private throwIfAnyNegativeNumbers(parsedNumbers: number[]) {
+    const negativeNumbers = parsedNumbers.filter((n) => n < 0);
+    if (negativeNumbers.length > 0) {
+      throw "negatives not allowed: " + negativeNumbers.join(",");
+    }
+  }
+
+  private parseNumbers(input: string, delimiter: string | RegExp) {
+    return input.split(delimiter).map((s) => Number.parseInt(s));
+  }
+
+  private removeCustomDelimiterHeader(input: string): string {
+    return input.slice(3);
+  }
+
+  private extractCustomDelimiter(input: string): string | RegExp {
+    return input[2];
+  }
+
+  private hasCustomDelimiter(input: string) {
+    return input.startsWith("//");
   }
 }
